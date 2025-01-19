348801e7b86096a3c37fbcde6c1ad2eee42b8069
SFIRegret19 <fspirin8@gmail.com>
2025-01-20 00:18:28 +0700

    Добавил юрл пути

--- GamingStoreApp/GamingStoreApp/urls.py
+++ GamingStoreApp/GamingStoreApp/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from task1.views import *
+from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('base/', base),
+    path('platform/', platform_page_index),
+    path('platform/cart', cart_page_index),
+    path('platform/catalog', catalog_page_index)
 ]
