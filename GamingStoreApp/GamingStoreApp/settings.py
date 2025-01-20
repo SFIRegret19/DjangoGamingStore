b41ff79583e7758652c339f8cb71daee642fb5dc
SFIRegret19 <fspirin8@gmail.com>
2025-01-21 04:29:25 +0700

    Подключение PGSQL DB

--- GamingStoreApp/GamingStoreApp/settings.py
+++ GamingStoreApp/GamingStoreApp/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'GamingStoreApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "GamingStoreAppDB",
+        "USER": "postgres",
+        "PASSWORD": "Postgre_fed_123",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
