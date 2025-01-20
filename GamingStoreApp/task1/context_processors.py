080e2210b3121303046f00d60694a429607f26a0
SFIRegret19 <fspirin8@gmail.com>
2025-01-21 01:17:21 +0700

    Добавил и создал context_processing, для отображения пунктов меню на всех шаблонах

---
+++ GamingStoreApp/task1/context_processors.py
@@ -0,0 +1,6 @@
+def menu_context(request):
+    return {
+        'platform_page_link_text': 'Главная',
+        'catalog_page_link_text': 'Магазин',
+        'cart_page_link_text': 'Корзина',
+    }
